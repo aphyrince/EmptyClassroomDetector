0af6d0ff1982a2472130034e628654be71546201
aphyrince <dkswjdgh01@naver.com>
2024-08-29 19:25:08 +0900

    프로젝트 초기 설정

---
+++ emptyclassroomdetector/src/main/java/main/emptyclassroomdetector/EmptyclassroomdetectorApplication.java
@@ -0,0 +1,13 @@
+package main.emptyclassroomdetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptyclassroomdetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmptyclassroomdetectorApplication.class, args);
+	}
+
+}
---
+++ emptyclassroomdetector/src/test/java/main/emptyclassroomdetector/EmptyclassroomdetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package main.emptyclassroomdetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptyclassroomdetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
